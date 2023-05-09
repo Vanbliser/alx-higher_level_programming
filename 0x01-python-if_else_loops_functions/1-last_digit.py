9fadfc567866ac473e363f17bf551db2f050c284
Blossom Ayogu <ayogublossomisrael@gmail.com>
2023-05-09 22:32:04 +0000

    Fix error output

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,7 +7,7 @@ if (number > 0):
 if ((number % div) > 5):
     print(f"Last digit of {number} is {number % div} and is greater than 5")
 if ((number % div) == 0):
-    print(f"Last digit of {number} is {number % div} and is zero")
+    print(f"Last digit of {number} is {number % div} and is 0")
 if ((number % div) < 6 and (number % div) != 0):
     print(f"Last digit of {number} is {number % div} and is less ", end="")
     print(f"than 6 and not 0")
