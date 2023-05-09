384a8bbf9217c6bfd6d2ecf81c6c3a560bcb9597
Blossom Ayogu <ayogublossomisrael@gmail.com>
2023-05-09 22:27:56 +0000

    Fix pep8 style error

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,7 +3,7 @@ import random
 number = random.randint(-10000, 10000)
 div = -10
 if (number > 0):
-    div = 10 
+    div = 10
 if ((number % div) > 5):
     print(f"Last digit of {number} is {number % div} and is greater than 5")
 if ((number % div) == 0):
