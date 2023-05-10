c8d2e4972f640b0736e9af0ca33c8019a302f60b
Blossom Ayogu <ayogublossomisrael@gmail.com>
2023-05-10 16:55:51 +0000

    Update 8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 
 def uppercase(str):
-    # A function that prints a string in uppercase followed by a new line.
-    
     for c in str:
         if ord('a') <= ord(c) <= ord('z'):
             c = chr(ord(c) - (ord('a') - ord('A')))
