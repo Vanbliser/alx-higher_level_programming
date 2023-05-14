49ed20ba65090935439e5a3300e225a75dd7d955
Blossom Ayogu <ayogublossomisrael@gmail.com>
2023-05-14 19:49:55 +0000

    Update 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,23 +1,18 @@
 #!/usr/bin/python3
-import sys
-from calculator_1 import add, sub, mul, div
-import argparse
-
-if __name__ == '__main__':
-    if len(sys.argv) != 4:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>", end="")
-        sys.exit("")
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
-    symbol = ord(sys.argv[2])
-    if symbol == 43:
-        print("{} {} {} = {}".format(a, sys.argv[2], b, add(a, b)))
-    elif symbol == 45:
-        print("{} {} {} = {}".format(a, sys.argv[2], b, sub(a, b)))
-    elif symbol == 42:
-        print("{} {} {} = {}".format(a, sys.argv[2], b, mul(a, b)))
-    elif symbol == 47:
-        print("{} {} {} = {}".format(a, sys.argv[2], b, div(a, b)))
+if __name__ == "__main__":
+    from sys import argv
+    if len(argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        quit(1)
+    a = int(argv[1])
+    b = int(argv[3])
+    ops = ["+", "-", "*", "/"]
+    from calculator_1 import add, sub, mul, div
+    funcs = [add, sub, mul, div]
+    for i, s in enumerate(ops):
+        if argv[2] == s:
+            print("{} {} {} = {}".format(a, s, b, funcs[i](a, b)))
+            break
     else:
-        print("Unknown operator. Available operators: +, -, * and /", end="")
-        sys.exit("")
+        print("Unknown operator. Available operators: +, -, * and /")
+        quit(1)
