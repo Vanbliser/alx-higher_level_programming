ee704c392264e64cc6d663a765d7a3e61667d571
Blossom Ayogu <ayogublossomisrael@gmail.com>
2023-06-01 13:23:02 +0000

    Add 3-safe_print_division.py

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-
+    c = None
+    try:
+        c = a/b
+    except Exception:
+        pass
+    finally:
+        print("Inside result: {}".format(c))
