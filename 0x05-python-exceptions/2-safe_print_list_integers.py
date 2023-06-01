c92678b228b437d913905f688108ef50f91292cb
Blossom Ayogu <ayogublossomisrael@gmail.com>
2023-06-01 13:10:17 +0000

    Add 2-safe_print_list_integers.py

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    i = 0
+    try:
+        while (i < x):
+            if (isinstance(my_list[i], int)):
+                print("{:d}".format(my_list[i]), end="")
+            i += 1
+        print()
+        return i
+    except Exception:
+        raise
