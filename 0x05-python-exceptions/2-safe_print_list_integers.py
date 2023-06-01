64cf5881373151500b7e55cccb991769a0542897
Blossom Ayogu <ayogublossomisrael@gmail.com>
2023-06-01 13:15:35 +0000

    Fix bug

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -6,7 +6,7 @@ def safe_print_list_integers(my_list=[], x=0):
         while (i < x):
             if (isinstance(my_list[i], int)):
                 print("{:d}".format(my_list[i]), end="")
-                count += 0
+                count += 1
             i += 1
         print()
         return count
