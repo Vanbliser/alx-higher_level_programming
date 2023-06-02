324c2218e781bf63b1b2a52a63938bc8c6a7525e
Blossom Ayogu <ayogublossomisrael@gmail.com>
2023-06-02 20:25:27 +0000

    Add 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""Contains a class Square
+
+"""
+
+
+class Square:
+    """A class Square that defines a square by
+
+    """
+
+    def __init__(self, size):
+        """Instantiate with a size
+
+        Args:
+            size: size of square
+        """
+        self.__size = size
