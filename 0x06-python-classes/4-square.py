f0c7fe92542f4464ec92af2dd9b4253c6a795241
Blossom Ayogu <ayogublossomisrael@gmail.com>
2023-06-03 08:04:47 +0000

    Add 4-square.py

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+"""Contains a class Square.
+
+"""
+
+
+class Square:
+    """A class square."""
+
+    def __init__(self, size=0):
+        """constructor to initialize a class."""
+        self.size = size
+
+    @property
+    def size(self):
+        """property getter size
+
+        size must be an integer. If size is less than 0, raise an exception
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not (isinstance(value, int)):
+            raise TypeError('size must be an integer')
+        elif value < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = value
+
+    def area(self):
+        """area of a suare.
+
+        Returns:
+            the area of square
+        """
+        return (self.size)**2
