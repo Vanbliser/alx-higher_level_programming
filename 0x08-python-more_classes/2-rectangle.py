36ec0e407a2c69ee91a7324bfe5ca460213b181d
Blossom Ayogu <ayogublossomisrael@gmail.com>
2023-06-15 09:13:05 +0000

    Fix bug in perimeter calculation

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -59,4 +59,4 @@ class Rectangle:
         """return the perimeter of the rectangle"""
         if (self.__width == 0) or (self.__height == 0):
             return 0
-        return (self.__width + self.__height)
+        return 2*(self.__width + self.__height)
