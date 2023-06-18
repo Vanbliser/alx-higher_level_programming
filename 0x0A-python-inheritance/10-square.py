ffc32c6d7d25fd4991d831847ba76e750c64f3f9
Blossom Ayogu <ayogublossomisrael@gmail.com>
2023-06-18 16:30:30 +0000

    Add 10-square.py

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""contain a class Square"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """a class Square"""
+    def __init__(self, size):
+        """class constuctor"""
+        super().__init__(size, size)
