876812b17fa68e8c0241e1889c27b1835215122b
Blossom Ayogu <ayogublossomisrael@gmail.com>
2023-06-19 01:48:34 +0000

    Add more tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -15,7 +15,20 @@ class Test(unittest.TestCase):
         r = Rectangle(5, 3)
         self.assertIsInstance(r, Rectangle)
 
-    def test_attributes(self):
+    def test_private_attribute_is_hidden(self):
+        r = Rectangle(5, 3)
+        self.assertEqual(hasattr(r, "__nb_objects"), False)
+
+    def test_attribute_id(self):
+        """test the instance attribute id"""
+        r = Rectangle(5, 3)
+        self.assertEqual(r.id, 1)
+        r1 = Rectangle(5, 3, id=12)
+        self.assertEqual(r1.id, 12)
+        r2 = Rectangle(5, 3)
+        self.assertEqual(r2.id, 2)
+
+    def test_public_attributes(self):
         """test correct attribute is passed"""
         r = Rectangle(5, 3, 2, 1, 12)
         self.assertEqual(r.id, 12)
