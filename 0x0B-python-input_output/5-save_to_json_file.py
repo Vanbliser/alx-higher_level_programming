c6bc640529adfbd26072165377da7b2f6626c1a7
Blossom Ayogu <ayogublossomisrael@gmail.com>
2023-06-26 22:29:33 +0000

    Task 5

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""contains def save_to_json_file(my_obj, filename) function"""
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """a function that writes an Object to a text file,
+    using a JSON representation
+
+    Args:
+        my_obj (obj): the object to write to file
+        filename (str): the name of the file
+    """
+    JSON = json.dumps(my_obj)
+    with open(filename, mode='w', encoding='utf-8') as file:
+        file.write(JSON)
