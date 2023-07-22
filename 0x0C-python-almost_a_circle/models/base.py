1f5b65c6402794e17b970129349901521ccc775d
Blossom Ayogu <ayogublossomisrael@gmail.com>
2023-07-22 14:23:45 +0000

    task 15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -14,3 +14,12 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """static method that returns the json representation
+        of list_dictionaries
+        """
+        if list_dictionaries is not None and len(list_dictionaries) > 0:
+            from json import dumps
+            return dumps(list_dictionaries)
