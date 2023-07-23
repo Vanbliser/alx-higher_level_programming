07fade35717149db3395d995dc2412bbe424b05e
Blossom Ayogu <ayogublossomisrael@gmail.com>
2023-07-23 11:23:30 +0000

    Advance task 1

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -26,6 +26,24 @@ class Base:
         return "[]"
 
     @staticmethod
+    def to_csv_string(list_dictionaries):
+        """static method that return a csv representation
+        of list_dictionatries
+        """
+        value = ""
+        if list_dictionaries is not None and len(list_dictionaries) > 0:
+            for i in list_dictionaries:
+                if "size" in i.keys():
+                    value += f"{i['id']},{i['size']},{i['x']},{i['y']}\n"
+                else:
+                    value += f"{i['id']},\
+                               {i['width']},\
+                               {i['height']},\
+                               {i['x']},\
+                               {i['y']}\n"
+        return value
+
+    @staticmethod
     def from_json_string(json_string):
         """static method that returns the list of the JSON string
         representation
@@ -34,6 +52,24 @@ class Base:
             return []
         from json import loads
         return loads(json_string)
+
+    @staticmethod
+    def from_csv_string(csv_string):
+        """static method that returns the list of csv string
+        representation
+        """
+        result = []
+        if csv_string is None or csv_string == "":
+            return result
+        for line in csv_string.splitlines():
+            values = list(map(int, line.split(',')))
+            if len(values) == 4:
+                keys = ['id', 'size', 'x', 'y']
+                result.append(dict(zip(keys, values)))
+            if len(values) == 5:
+                keys = ['id', 'width', 'height', 'x', 'y']
+                result.append(dict(zip(keys, values)))
+        return result
 
     @classmethod
     def save_to_file(cls, list_objs):
@@ -48,17 +84,14 @@ class Base:
                 f.write(Base.to_json_string(list_of_dict))
 
     @classmethod
-    def create(cls, **dictionary):
-        """class method that returns an instance with all attribute set"""
-        if cls.__name__ == "Square":
-            args = [1]
-        elif cls.__name__ == "Rectangle":
-            args = [1, 1]
-        else:
-            raise TypeError("Unsupported Type")
-        obj = cls(*args)
-        obj.update(**dictionary)
-        return obj
+    def save_to_file_csv(cls, list_objs):
+        """serialize to csv and save"""
+        with open(f"{cls.__name__}.csv", "w") as f:
+            if list_objs is None:
+                f.write(Base.to_csv_string(None))
+            else:
+                list_of_dict = [obj.to_dictionary() for obj in list_objs]
+                f.write(Base.to_csv_string(list_of_dict))
 
     @classmethod
     def load_from_file(cls):
@@ -73,3 +106,30 @@ class Base:
                 return list_of_obj
         else:
             return []
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """load from a csv file and deserialize"""
+        import os
+        if os.path.exists(f"{cls.__name__}.csv"):
+            with open(f"{cls.__name__}.csv", "r") as f:
+                list_of_obj_rep = Base.from_csv_string(f.read())
+                list_of_obj = []
+                for obj in list_of_obj_rep:
+                    list_of_obj.append(cls.create(**obj))
+                return list_of_obj
+        else:
+            return []
+
+    @classmethod
+    def create(cls, **dictionary):
+        """class method that returns an instance with all attribute set"""
+        if cls.__name__ == "Square":
+            args = [1]
+        elif cls.__name__ == "Rectangle":
+            args = [1, 1]
+        else:
+            raise TypeError("Unsupported Type")
+        obj = cls(*args)
+        obj.update(**dictionary)
+        return obj
