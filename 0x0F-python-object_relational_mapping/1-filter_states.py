5e84241669a626c4c28c1dde36524a08af34a7b7
Blossom Ayogu <ayogublossomisrael@gmail.com>
2023-08-26 02:05:39 +0000

    Add task 1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+
+"""a script that lists all states from the database hbtn_0e_0_usa"""
+
+
+if __name__ == '__main__':
+    import MySQLdb
+
+    from sys import argv
+
+    uname = argv[1]
+    passd = argv[2]
+    db_n = argv[3]
+    conn = MySQLdb.connect(host="localhost",
+                           port=3306,
+                           user=uname,
+                           passwd=passd,
+                           db=db_n,
+                           charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE `name` LIKE 'N%' ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
