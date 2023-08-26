503946c76c09ffd80bd9a8f02c12becfccf34c6b
Blossom Ayogu <ayogublossomisrael@gmail.com>
2023-08-26 03:01:01 +0000

    Add task 4

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""a script that lists all cities from the database hbtn_0e_4_usa"""
+
+
+if __name__ == "__main__":
+
+    import MySQLdb
+
+    from sys import argv
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3], charset="utf8")
+    cursor = db.cursor()
+    cursor.execute("""
+                        SELECT cities.id, cities.name, states.name
+                        FROM cities
+                        JOIN states
+                        ON cities.state_id = states.id
+                        ORDER BY cities.id
+                    """)
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    db.close()
