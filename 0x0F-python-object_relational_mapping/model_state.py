0fbc012b1d6aceb67632830842c423c4158d2f4e
Blossom Ayogu <ayogublossomisrael@gmail.com>
2023-08-26 03:47:30 +0000

    Add test files and fix sqlalchemy version bug

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -5,7 +5,7 @@
 import sqlalchemy
 
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
 
