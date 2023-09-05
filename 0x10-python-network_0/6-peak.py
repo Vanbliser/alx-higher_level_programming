b22afa71095fb8f6262186e4c4defe462e8f0184
Blossom Ayogu <ayogublossomisrael@gmail.com>
2023-09-05 23:29:30 +0000

    task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,7 +2,7 @@
 """contains a function that finds a peak in a list of unsorted integers."""
 
 
-def find_peak(list_of_integers):
+def find_peak_old(list_of_integers):
     """a function that finds a peak in a list of unsorted integers."""
     length = len(list_of_integers)
     if length == 0:
@@ -15,3 +15,23 @@ def find_peak(list_of_integers):
     p2 = find_peak(list_of_integers[a:b])
     p3 = find_peak(list_of_integers[b:])
     return max(p1, p2, p3)
+
+
+def find_peak(list_of_integers):
+    """a function that finds a peak in a list of unsorted integers."""
+    def find_peak_recursive(arr, left, right):
+        """a recursive function to find the peak"""
+        if left == right:
+            return arr[left]
+
+        mid = (left + right) // 2
+
+        if arr[mid] < arr[mid + 1]:
+            return find_peak_recursive(arr, mid + 1, right)
+        else:
+            return find_peak_recursive(arr, left, mid)
+
+    if len(list_of_integers) == 0:
+        return None
+
+    return find_peak_recursive(list_of_integers, 0, len(list_of_integers) - 1)
