8091a4f3c9210cb2e7953ca378de8ac556cb824a
Blossom Ayogu <ayogublossomisrael@gmail.com>
2023-09-05 22:53:28 +0000

    task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -7,6 +7,8 @@ def find_peak(list_of_integers):
     length = len(list_of_integers)
     if length == 0:
         return
+    if length == 1:
+        return list_of_integers[0]
     if length == 2:
         if list_of_integers[1] >= list_of_integers[0]:
             return list_of_integers[1]
@@ -18,9 +20,13 @@ def find_peak(list_of_integers):
         if (list_of_integers[0] >= list_of_integers[2]):
             return list_of_integers[0]
         return list_of_integers[2]
-    a = length // 2
+    a = length // 3
+    b = length - (2 * a) + 1
     p1 = find_peak(list_of_integers[:a])
-    p2 = find_peak(list_of_integers[a:])
-    if p1 >= p2:
+    p2 = find_peak(list_of_integers[a:b])
+    p3 = find_peak(list_of_integers[b:])
+    if p2 > p1 and p2 > p3:
+        return p2
+    if p1 >= p3:
         return p1
-    return p2
+    return p3
