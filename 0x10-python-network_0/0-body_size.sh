af9569e1a8e88b91373314882af8be3afa90fe89
Blossom Ayogu <ayogublossomisrael@gmail.com>
2023-09-05 21:03:56 +0000

    task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -2,4 +2,4 @@
 # a Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
 
 URL=$1
-curl -so /dev/null -w '%{size_download}\n' $URL
+curl -so /dev/null -w '%{size_download}\n' "$URL"
